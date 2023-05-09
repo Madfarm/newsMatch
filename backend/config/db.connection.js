24987413e5f019cca4e7418b0af03c0042c39a95
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-09 11:40:08 -0400

    Establish connection to database and add express boilerplate

---
+++ backend/config/db.connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const {MONGODB_URI} = process.env
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect(MONGODB_URI)
+
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
+  
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-const express = require('express');
+require('dotenv').config()
+require('./config/db.connection')
+
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+
+
+const app = express()
+
+const { PORT } = process.env 
+
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(cors())
+app.use(morgan("dev"))
+
+// routers will go here
+
+
+
+app.get('/', (req,res)=>res.send('Hitting home route'))
+
+
+
+app.listen(PORT, ()=>console.log(`Listening on PORT: ${PORT}`))
