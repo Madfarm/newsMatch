aaf9dcb470e4ae97d029009ad10f2d39e771317e
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-10 09:18:51 -0400

    Setup article page layout

---
+++ frontend/app/articles/page.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRight, faArrowLeft, faUpRightFromSquare, faArrowDown } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react";
+
+
+
+
+
+
+
+export default function ArticlesPage(props) {
+    // const articles = props.params.articles.news
+    const [carouselIdx, setCarouselIdx] = useState(10)
+    const [movement, setMovement] = useState(null);
+
+
+
+
+    function handleNavClick(direction) {
+        if (direction == "right") {
+            setCarouselIdx(carouselIdx + 1)
+            setMovement(direction)
+        } else {
+            setCarouselIdx(carouselIdx - 1)
+            setMovement(direction)
+        }
+    }
+
+    // const article = articles[carouselIdx];
+
+    return (
+        <main>
+            <article className={movement == 'right' ? "slide-right" : "slide-left"}>
+                <div className="article-section art-img">
+                    img
+                </div>
+                <div className="article-section art-desc">desc</div>
+
+                <a >
+                    <div className="article-section art-title">
+
+                        <h2>title</h2>
+                        <FontAwesomeIcon icon={faUpRightFromSquare} size={"2xl"} />
+
+                    </div>
+                </a>
+                <div className="article-section art-nav">
+                    <div onClick={() => handleNavClick('left')}>
+                        <FontAwesomeIcon  icon={faArrowLeft} style={{ color: "#ffffff" }} size={"2xl"} />
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faArrowDown} style={{ color: "#ffffff" }} size={"2xl"} />
+                    </div>
+                    <div onClick={() => handleNavClick('right')}>
+                        <FontAwesomeIcon  icon={faArrowRight} style={{ color: "#ffffff" }} size={"2xl"} />
+                    </div>
+                </div>
+            </article>
+        </main>
+    )
+}
