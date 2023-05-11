c55d974db692e01508840bbf6adeb81f1c83d5ac
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-11 15:30:33 -0400

    Finish the landing page - mobile responsive

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,9 +1,13 @@
-
+import Link from "next/link"
 
 export default function HomePage(){
     return (
-        <main>
-            <h1>Hello world!</h1>
+        <main id="homepage">
+            <h1>Fall in love with the news again</h1>
+            <p>News Match gives you all the news you love with the convenience of a dating app</p>
+            <Link href="/articles">
+                <button id="get-started-btn">Get Started</button>
+            </Link>
         </main>
     )
 }
