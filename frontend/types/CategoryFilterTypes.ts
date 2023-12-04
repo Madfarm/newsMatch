d8488867ccff1ea9fbf1859eaa145285788ef177
AnthonyScott <anthony.scott1005@gmail.com>
2023-12-04 07:58:53 -0500

    Create enum for valid categories

---
+++ frontend/types/CategoryFilterTypes.ts
@@ -0,0 +1,19 @@
+enum validCategories {
+    all,
+    regional,
+    lifestyle,
+    business,
+    general,
+    programming,
+    science,
+    entertainment,
+    world,
+    sports,
+    finance,
+    academia,
+    poltiics,
+    health,
+    opinion,
+    food,
+    game
+}
